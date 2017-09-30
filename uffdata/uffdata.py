26d388770aeaf04792fe2690a98c3471f84de587
Gustavo Alexandre <gassantos@id.uff.br>
2017-09-30 17:42:27 -0300

    Adjustments

--- uffdata/uffdata.py
+++ uffdata/uffdata.py
@@ -1,11 +1,17 @@
 #%config IPCompleter.greedy = True
-import pymysql
-conn=pymysql.connect(host="localhost", user="uffdata", passwd="uffdata123", db="dwebd152")
-uffdata = conn.cursor()
+#import pymysql
+import numpy as np 
 
-sqlCurso = 'SELECT * from BI_DMCURSO;'
-curso = uffdata.execute(sqlCurso)
-print("Number of Curses at UFF: ", curso)
+#conn=pymysql.connect(host="localhost", user="uffdata", passwd="uffdata123", db="dwebd152")
+#uffdata = conn.cursor()
 
-data = uffdata.fetchone()
-print(data)
+#sqlCurso = 'SELECT * from BI_DMCURSO;'
+#curso = uffdata.execute(sqlCurso)
+#print("Number of Curses at UFF: ", curso)
+
+#data = uffdata.fetchone()
+#print(data)
+
+a = np.random.randn(2, 3) # a.shape = (2, 3)
+b = np.random.randn(2, 1) # b.shape = (2, 1)
+c = a + b
