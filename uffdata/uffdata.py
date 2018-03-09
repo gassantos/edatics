4403c383ee279194929c435032c1f66d4eb7d460
Gustavo Alexandre <gassantos@id.uff.br>
2018-03-08 22:58:43 -0300

    Adicionando análise de desempenho acadêmico em grandes eventos.

--- uffdata/uffdata.py
+++ uffdata/uffdata.py
@@ -1,17 +1,11 @@
 #%config IPCompleter.greedy = True
-#import pymysql
-import numpy as np 
+import pymysql
+conn=pymysql.connect(host="localhost", user="uffdata", passwd="uffdata123", db="dwebd152")
+uffdata = conn.cursor()
 
-#conn=pymysql.connect(host="localhost", user="uffdata", passwd="uffdata123", db="dwebd152")
-#uffdata = conn.cursor()
+sqlCurso = 'SELECT * from BI_DMCURSO;'
+curso = uffdata.execute(sqlCurso)
+print("Number of Curses at UFF: ", curso)
 
-#sqlCurso = 'SELECT * from BI_DMCURSO;'
-#curso = uffdata.execute(sqlCurso)
-#print("Number of Curses at UFF: ", curso)
-
-#data = uffdata.fetchone()
-#print(data)
-
-a = np.random.randn(2, 3) # a.shape = (2, 3)
-b = np.random.randn(2, 1) # b.shape = (2, 1)
-c = a + b
+data = uffdata.fetchone()
+print(data)
